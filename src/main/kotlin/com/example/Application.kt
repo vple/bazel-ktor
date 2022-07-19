cb0ea5509d1d8b358750a3dde07bfc46bdaa5c8c
Vincent Le <vince1123581321@gmail.com>
2022-07-19 19:25:40 -0400

    Add jvm jar

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,7 +1,9 @@
 package com.example
 
+import com.google.common.collect.Lists
 import io.ktor.server.application.Application
 
 fun main(args: Array<String>) {
     println("Hello world!")
+    println(Lists.newArrayList("Hello", "world!").joinToString(" "))
 }
